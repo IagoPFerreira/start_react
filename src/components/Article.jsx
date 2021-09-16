f921944fedd795f3635fb05693707529043cc539
iago <iago.pferreiravr@gmail.com>
2021-09-16 12:03:34 -0300

    Feat: implementação de renderizacao da imagem no componente Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 class Article extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {};
+  }
+
+  showImage = () => {
+    const { image, title, storyline } = this.props;
+    // Logica
+    return (
+      <img src={image} alt="" />
+    )
+  }
+
   render() {
+    const { image, title, storyline } = this.props;
+    console.log(image);
+    
     return (
       <article>
-        
+      { image && this.showImage() }
+      {}
       </article>
     );
   }
 }
+
+Article.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  storyline: PropTypes.string,
+}.isRequired;
 
 export default Article;
