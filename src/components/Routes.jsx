214c2cc06130a0759e30d21b5d79fa0c7c79e4e3
iago <iago.pferreiravr@gmail.com>
2021-09-16 11:19:58 -0300

    Feat: definição do esqueleto inicial para uso de rotas

---
+++ src/components/Routes.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
