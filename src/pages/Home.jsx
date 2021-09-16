e18bb045550638ef1dd5b193bd6fec7151e4c188
iago <iago.pferreiravr@gmail.com>
2021-09-16 11:20:51 -0300

    Feat: pagina Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1 />
+          <img />
+          <nav />
+        </header>
+        <aside>
+          <a/>
+          <a/>
+          <a/>
+        </aside>
+        <main>
+          <article />
+          <article />
+          <article />
+        </main>
+        <footer />
+      </>
+    );
+  }
+}
+
+export default Home;
