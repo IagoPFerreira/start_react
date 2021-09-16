c415aed87996dc6f42c2e52198cff9ae8919789d
iago <iago.pferreiravr@gmail.com>
2021-09-16 11:21:01 -0300

    Feat: componente Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Start React</h1>
+        <img />
+        <nav />
+      </header>
+    );
+  }
+}
+
+export default Header;
